b340c76bcd08877733a3363fc71668d39ab8d8cc
IlhamShiddiq <shdqillham123@gmail.com>
2024-09-12 09:04:21 +0700

    fix: attack sql query

--- files/attack.sql
+++ files/attack.sql
@@ -1,11 +1,12 @@
 CREATE TABLE public.attacks (
     id serial4 NOT NULL,
-    sourceCountry      varchar   NOT NULL,
-    destinationCountry varchar,
-    millisecond        integer   NOT NULL,
-    type               varchar,
-    weight             varchar   NOT NULL,
-    attackTime         timestamptz NOT NULL,
+    sourceCountry      varchar       NULL,
+    destinationCountry varchar      NULL,
+    millisecond        integer       NULL,
+    type               varchar      NULL,
+    weight             varchar      NULL,
+    attackTime         timestamptz  NULL,
+
     CONSTRAINT attacks_pkey PRIMARY KEY (id)
 );
 
